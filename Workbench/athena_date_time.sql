e56c68796af17e68722e8aa04bd89f9d7da4d148
Chad Meeks <chad.michael.meeks@gmail.com>
2023-01-06 12:35:18 -0800

    added some queries

--- Workbench/athena_date_time.sql
+++ Workbench/athena_date_time.sql
@@ -44,26 +44,37 @@ SELECT date_diff('day', current_date, current_date - interval '2' day);
 SELECT localtimestamp, localtimestamp - interval '2' day;
 SELECT date_diff('day', localtimestamp, localtimestamp - interval '2' day);
 
-
--- MA Timestamp
-SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_timestamp FROM "production_ma_analytics_tables"."transactions" 
-LIMIT 10;
-
-
-SELECT retailer_transaction_timestamp_utc, localtimestamp FROM "production_ma_analytics_tables"."transactions";
-
-SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_time_stamp, date_diff('day', retailer_transaction_timestamp_utc, localtimestamp) as diff FROM "production_ma_analytics_tables"."transactions"
-LIMIT 10;
-
-SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_time_stamp, date_diff('day', retailer_transaction_timestamp_utc, localtimestamp) as diff FROM "production_ma_analytics_tables"."transactions"
-WHERE diff = 5000
-LIMIT 10;
-
-SELECT title, retailer_transaction_timestamp_utc FROM "production_ma_analytics_tables"."transactions"
-WHERE extract(YEAR FROM retailer_transaction_timestamp_utc) = 2022
-LIMIT 10;
+-- Extract
 
 SELECT extract(YEAR FROM localtimestamp);
 
-SELECT title, retailer_transaction_timestamp_utc, extract(YEAR FROM retailer_transaction_timestamp_utc) FROM "production_ma_analytics_tables"."transactions"
+
+-- MA Timestamp
+SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_timestamp FROM "production_anon_ma_analytics_tables"."transactions" 
 LIMIT 10;
+
+
+SELECT retailer_transaction_timestamp_utc, localtimestamp FROM "production_anon_ma_analytics_tables"."transactions"
+LIMIT 10;
+
+-- Date Diff
+
+SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_time_stamp, date_diff('day', retailer_transaction_timestamp_utc, localtimestamp) as days_diff FROM "production_ma_analytics_tables"."transactions"
+LIMIT 10;
+
+SELECT title, retailer_transaction_timestamp_utc, localtimestamp as local_time_stamp, date_diff('day', retailer_transaction_timestamp_utc, localtimestamp) as days_diff FROM "production_ma_analytics_tables"."transactions"
+WHERE date_diff('day', retailer_transaction_timestamp_utc, localtimestamp) < 1000
+LIMIT 10;
+
+-- Extract
+
+SELECT title, retailer_transaction_timestamp_utc, extract(YEAR FROM retailer_transaction_timestamp_utc) FROM "production_anon_ma_analytics_tables"."transactions"
+LIMIT 1000;
+
+SELECT title, retailer_transaction_timestamp_utc FROM "production_anon_ma_analytics_tables"."transactions"
+WHERE extract(YEAR FROM retailer_transaction_timestamp_utc) = 2021
+LIMIT 10;
+
+SELECT distinct(extract(YEAR FROM retailer_transaction_timestamp_utc)), count(*) FROM "production_anon_ma_analytics_tables"."transactions"
+GROUP BY 1
+ORDER BY 1 DESC
